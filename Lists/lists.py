05eb5b50b1e9b1885f2f2b01187f487260416eed
HarounTaHa <harountaha@outlook.sa>
2022-10-24 14:29:21 +0300

    section lists :)

---
+++ Lists/lists.py
@@ -0,0 +1,26 @@
+test_list = ['Hello', 'My name is', 'Haroun']
+print(test_list)
+# ['Hello', 'My name is', 'Haroun']
+print(len(test_list))
+# 3
+test_list.append(25)
+print(test_list)
+# ['Hello', 'My name is', 'Haroun', 25]
+test_list.pop()
+print(test_list)
+# ['Hello', 'My name is', 'Haroun']
+abc_list = ['b', 'c', 'a']
+print(abc_list)
+# ['b', 'c', 'a']
+abc_list.sort()
+print(abc_list)
+# ['a', 'b', 'c']
+num_list = [3, 2, 1]
+print(num_list)
+# [3, 2, 1]
+num_list.sort()
+print(num_list)
+# [1, 2, 3]
+num_list.reverse()
+print(num_list)
+# [3, 2, 1]
