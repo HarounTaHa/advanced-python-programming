ac81352b6789e3e0f08398e9d43edcf6df44e3a2
HarounTaHa <harountaha@outlook.sa>
2022-10-25 06:09:40 +0300

    section slicing and index :)

---
+++ SlicingIndexing/sliceIndex.py
@@ -0,0 +1,23 @@
+name = 'Haroun'
+
+print(name[1])
+
+initial = name[0]
+
+print(initial)
+
+test = name[-1]
+
+print(test)
+
+greet = 'My name is Haroun'
+
+print(greet[3:7])
+print(greet[1::2])
+
+test_list = [5, 10, 20]
+print(test_list[0] + 5)
+
+test_list = ['Johan', 5, 10]
+
+print(test_list[0][2])
