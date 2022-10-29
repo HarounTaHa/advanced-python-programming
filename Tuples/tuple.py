117774429a950986bf8951e05a20206556c5f9ab
HarounTaHa <harountaha@outlook.sa>
2022-10-29 09:33:30 +0200

    section Tuples :)

---
+++ Tuples/tuple.py
@@ -0,0 +1,14 @@
+test_tup = ('one', 2, 5.67)
+
+print(test_tup[0])
+print(test_tup[1])
+print(test_tup[2])
+print('Index 2:', test_tup.index(2))
+print(test_tup[1])
+
+test_tup = (1, 2, 3, 4, 1, 2)
+print(test_tup)
+
+count_1 = test_tup.count(1)
+print(count_1)
+print(test_tup.count(5))
