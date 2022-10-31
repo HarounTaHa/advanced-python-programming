762a571f0957ecfa52e9d82eb3e6ece24b0a83c7
HarounTaHa <harountaha@outlook.sa>
2022-10-31 15:26:07 +0200

    section Sets :)

---
+++ Sets/set.py
@@ -0,0 +1,20 @@
+test_list = [5, 6, 7, 8, 5]
+print(test_list)
+
+test_set = set()
+test_set.add(5)
+test_set.add(6)
+test_set.add(7)
+test_set.add(8)
+print(test_set)
+test_set.add(5)
+print(test_set)
+
+second_set = set()
+second_set.add('Hello')
+print(second_set)
+second_set.add('Hello')
+print(second_set)
+second_set.add('hello')
+print(second_set)
+
